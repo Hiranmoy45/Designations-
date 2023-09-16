a0cb7b51979a34dd09210ad36f09766d63d16cfd
Hiranmoy <hiranmoyhs380@gmail.com>
2023-09-16 19:14:16 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from 'next/image'
-import Link from 'next/link'
+
+
 
 export default function Home() {
   return (
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="flex-1 flex flex-col items-center justify-center">
           <div className="bg-white bg-opacity-10 px-4 py-2 rounded-xl flex items-center justify-center text-cyan-100 space-x-2 lg:space-x-4">
 
-
+          <img src="/destination.png" className="w-52 " alt=" Logo"/>  
           </div>
           <h1 className="text-6xl lg:text-7xl xl:text-8xl text-gray-200 tracking-wider font-bold font-serif mt-12 text-center">Coming Soon</h1>
           <div className="flex flex-col items-center space-y-4 mt-24">
